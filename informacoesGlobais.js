7febbc0cb4724b7227baa3362d04165b44aa22cf
ArthurMatheusBileski <162511010+ArthurMatheusBileski@users.noreply.github.com>
2024-11-13 21:44:33 -0300

    Update informacoesGlobais.js

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -18,7 +18,7 @@ const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixe
     paragrafo.classList.add("graficos-container__texto");
     paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}</span> bilhões de pessoas e que aproximadamente <span>${pessoasConectadas}</span> bilhões estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> pessoas conectadas em alguma rede social.`
 
-    paragrafo.innerHTML = ``
+    paragrafo.innerHTML = `Cerca de 38% das mais de 7 bilhões de pessoas do mundo estão no mercado de trabalho, ou seja, aproximadamente 3 bilhões de trabalhadores. Do total de trabalhadores, ⅔ deles (67%) precisam de transporte para o trabalho. Dessa forma, uma porcentagem bastante relevante (os outros 34%) estão em teletrabalho ou moram perto do trabalho, de forma a não precisar de deslocamento. Entre os que precisam de transporte, o tempo necessário para isso é de cerca de 54 minutos. Esse tempo pode ser utilizado para ouvir PodCasts ou videoaulas, caso o transporte seja público ou por aplicativo. Um serviço que pode ser oferecido é o de Podcasts com duração compatível com esse tempo de deslocamento.`
 
     
     
